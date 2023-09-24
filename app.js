fc9594322a3949ca31d2ba4da2fc80baeb19fd21
Mariojoestar33 <mariano.pena15@gmail.com>
2023-09-24 17:52:08 -0600

    sql

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+//Creacion de variables de aplicacion y dependencias
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 
-const host = "127.0.0.1"
-const port = 3000
-const http = require('http')
-const fs = require('fs')
+app.use(express.static('public')) //Asignacion de ruta de hojas estaticas
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('public/index.html', (err, data) => {
-        if(err) {
-            res.writeHead(404)
-            res.write('Error 404: No resources found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
+//Ruta de la pagina principal
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, 'public', 'index.html')
 })
 
-server.listen(port, host, () => {
-    console.log('Servidor en http://',host,':',port)
-});
+//Conexion y creacion de la base de datos
+
+const db = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'rincon'
+})
+
+db.connect( (err) => {
+    if(err) {
+        console.error('Error de conexion: ' + err.message)
+    }
+    console.log('¡¡Conexion exitosa!!')
+})
+
+//Chequeo del estado del servidor
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000')
+})
