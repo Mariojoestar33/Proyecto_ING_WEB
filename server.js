1e7f5d9a72b62cbf0dbbab9d27588a6013ea6eba
Mariojoestar33 <mariano.pena15@gmail.com>
2023-10-03 19:09:56 -0600

    ejs

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.use(express.static('public'))
+// Configurar EJS como motor de plantillas
+app.set('view engine', 'ejs');
 
+// Establecer la ubicación de las plantillas EJS
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware para servir archivos estáticos (como CSS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para la página de inicio (index.ejs)
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+  res.render('index', {
+    pageTitle: 'Rincon Paramo',
+    currentYear: new Date().getFullYear(),
+  });
+});
 
+// Ruta para la página de inicio de sesión (login.ejs)
+app.get('/login', (req, res) => {
+  res.render('login', {
+    pageTitle: 'Iniciar Sesión',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+// Ruta para la página de creación de cuentas (creacion.ejs)
+app.get('/creacion', (req, res) => {
+  res.render('creacion', {
+    pageTitle: 'Crear Cuenta',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+// Iniciar el servidor en el puerto 3000
 app.listen(3000, () => {
-    console.log('Servidor montado en el puerto 3000')
-})
+  console.log('Servidor en ejecución en el puerto 3000');
+});
